7e09026b82e0d8163fc03a454224c501df1c437b
Leon Kleinschmidt <leon.kleinschmidt@outlook.de>
2025-10-13 23:28:24 +0200

    add action & docker container

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # CS50 CI Action
+
+```docker build -t cs50-ci-action .```
+```
+docker run --rm cs50-ci-action \
+  12345 \
+  67890 \
+  "b64" \
+  "HSDDigitalLabor/problems" \
+  "HSDDigitalLabor/solutions"
+```
---
+++ src/main.py
@@ -0,0 +1,11 @@
+import sys
+
+if __name__ == "__main__":
+    app_id = sys.argv[1]
+    install_id = sys.argv[2]
+    private_key = sys.argv[3]
+    problems_repo = sys.argv[4]
+    solutions_repo = sys.argv[5]
+
+    print("App ID:", app_id)
+    print("Install ID:", install_id)
